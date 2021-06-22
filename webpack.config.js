9d510c905d1761e1c225614e034a20f251ad8632
powerar <arpower@bell.net>
2021-06-22 19:42:49 -0400

    optimized images and split events, schedules, tickets, and app

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,54 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 //create the main configuration object
 //requires 3 objects: entry, output, and mode
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
-    //add the jquery plugins
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        }),
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name(file) {
+                return '[path][name].[ext]';
+              },
+              publicPath: function (url) {
+                return url.replace('../', '/assets/');
+              },
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
     ],
-    mode: 'development'
+  },
+  //add the jquery plugins
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+    }),
+  ],
+  mode: 'development',
 };
